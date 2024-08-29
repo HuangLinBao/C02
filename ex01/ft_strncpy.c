53c612cb39d819a9b7e81bcf86783de42f0542f4
Huanglinbao <tariq.92@live.com>
2024-08-29 13:24:22 +0400

    ssdghdh

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: tsabri <tsabri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 10:04:37 by tsabri            #+#    #+#             */
-/*   Updated: 2024/08/29 13:15:41 by tsabri           ###   ########.fr       */
+/*   Updated: 2024/08/29 13:24:03 by tsabri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,26 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	unsigned int		i;
 
 	i = 0;
-	while (i < n && src[i] != '\0' )
+	while (i <= n && src[i] != '\0' )
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	if (dest[i] != '\0' && src[i] == '\0')
+	while (i < n)
+	{
 		dest[i] = '\0';
+		i++;
+	}
 	return (dest);
 }
 
-// int	main(void)
-// {
-// 	char src[] = "Hello";
-// 	char dest[] = "World1";
-// 	char dest1[] = "World1";
+int	main(void)
+{
+	char src[] = "Hello";
+	char dest[] = "World1";
+	char dest1[] = "World1";
 
-// 	printf("%s\n", ft_strncpy(dest, src, 7));
-// 	printf("%s\n", strncpy(dest1, src, 7));
-// 	return (0);
-// }
+	printf("%s\n", ft_strncpy(dest, src, 5));
+	printf("%s\n", strncpy(dest1, src, 5));
+	return (0);
+}

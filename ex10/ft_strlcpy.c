2369cfd6ff37c410c791b457b3d15a4fad599e89
Huanglinbao <tariq.92@live.com>
2024-08-29 08:59:57 +0400

    oi

--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: tsabri <tsabri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 17:20:21 by tsabri            #+#    #+#             */
-/*   Updated: 2024/08/28 18:07:29 by tsabri           ###   ########.fr       */
+/*   Updated: 2024/08/29 08:59:35 by tsabri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,29 +27,40 @@ unsigned int	ft_strlen(char *str)
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
-	int	i;
+	unsigned int	i;
 
 	if (ft_strlen(src) == 0)
-		return (0);
-	if (size == 0)
-		return (ft_strlen(src));
-	else if (size >= ft_strlen(src))
-	{
-		i = 0;
-		while (src[i])
-		{
-			dest[i] = src[i];
-			i++;
-		}
-		return (ft_strlen(src));
-	}
-	else
+		dest = "";
+	else if (size < ft_strlen(src))
 	{
 		i = 0;
 		while (i < size)
 		{
 			dest[i] = src[i];
+			i++;
 		}
-		return (ft_strlen(src));
+		dest[i] = '\0';
 	}
+	else
+	{
+		i = 0;
+		while (src[i] != '\0')
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (ft_strlen(src));
 }
+
+// int	main(void)
+// {
+// 	char src[] = "Hi";
+// 	char dest[5];
+// 	char *s = &src[0];
+// 	char *d = &dest[0];
+// 	int len = ft_strlcpy(d, s, 0);
+// 	printf("size is: %d\nDest is %s\n", len, dest);
+// 	return (0);
+// }
